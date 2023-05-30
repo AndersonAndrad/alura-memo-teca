204dfbf8411f5fe42a08d66b461d2ef60cd710ab
AndersonAndrad <anderson_andrade_@outlook.com>
2023-05-30 19:25:25 -0300

    feat: pass information component to template

--- src/app/components/think/create-think/create-think.component.ts
+++ src/app/components/think/create-think/create-think.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-think',
   templateUrl: './create-think.component.html',
-  styleUrls: ['./create-think.component.scss']
+  styleUrls: ['./create-think.component.scss'],
 })
 export class CreateThinkComponent {
+  think: Think = {
+    id: '1',
+    content: 'Aprendendo angular',
+    autor: 'dev',
+    model: '',
+  };
+}
 
+interface Think {
+  id: string;
+  content: string;
+  model: string;
+  autor: string;
 }
